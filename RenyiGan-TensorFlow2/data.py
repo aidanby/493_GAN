b26d5e921d92cf33e1453c48090eb60aed57cd6a
aidanby <aidanbyang@hotmail.com>
2021-02-02 14:23:02 -0700

    new data prep

--- RenyiGan-TensorFlow2/data.py
+++ RenyiGan-TensorFlow2/data.py
@@ -5,6 +5,7 @@ import mido
 import string
 import numpy as np
 import matplotlib.pyplot as plt
+import os
 
 def mid2arry(mid, min_msg_pct=0.1):
     tracks_len = [len(tr) for tr in mid.tracks]
@@ -19,7 +20,7 @@ def mid2arry(mid, min_msg_pct=0.1):
     max_len = max([len(ary) for ary in all_arys])
     for i in range(len(all_arys)):
         if len(all_arys[i]) < max_len:
-            all_arys[i] += [[0] * 88] * (max_len - len(all_arys[i]))
+            all_arys[i] += [[0] * 128] * (max_len - len(all_arys[i]))
     all_arys = np.array(all_arys)
     all_arys = all_arys.max(axis=0)
     # trim: remove consecutive 0s in the beginning and at the end
@@ -45,10 +46,9 @@ def msg2dict(msg):
     return [result, on_]
 
 def switch_note(last_state, note, velocity, on_=True):
-    # piano has 88 notes, corresponding to note id 21 to 108, any note out of this range will be ignored
-    result = [0] * 88 if last_state is None else last_state.copy()
-    if 21 <= note <= 108:
-        result[note-21] = velocity if on_ else 0
+    result = [0] * 128 if last_state is None else last_state.copy()
+    # if 21 <= note <= 108:
+    result[note] = velocity if on_ else 0
     return result
 
 def get_new_state(new_msg, last_state):
@@ -57,9 +57,8 @@ def get_new_state(new_msg, last_state):
     return [new_state, new_msg['time']]
 
 def track2seq(track):
-    # piano has 88 notes, corresponding to note id 21 to 108, any note out of the id range will be ignored
     result = []
-    last_state, last_time = get_new_state(str(track[0]), [0]*88)
+    last_state, last_time = get_new_state(str(track[0]), [0]*128)
     for i in range(1, len(track)):
         new_state, new_time = get_new_state(track[i], last_state)
         if new_time > 0:
@@ -69,7 +68,7 @@ def track2seq(track):
 
 def arry2mid(ary, tempo=500000):
     # get the difference
-    new_ary = np.concatenate([np.array([[0] * 88]), np.array(ary)], axis=0)
+    new_ary = np.concatenate([np.array([[0] * 128]), np.array(ary)], axis=0)
     changes = new_ary[1:] - new_ary[:-1]
     # create a midi file with an empty track
     mid_new = mido.MidiFile()
@@ -98,14 +97,38 @@ def arry2mid(ary, tempo=500000):
     return mid_new
 
 def read_midi():
-    mid = mido.MidiFile('test.mid', clip=True)
-    result_array = mid2arry(mid)
-    plt.plot(range(result_array.shape[0]), np.multiply(np.where(result_array > 0, 1, 0), range(1, 89)), marker='.',
-             markersize=1, linestyle='')
-    plt.title("test.mid")
-    #plt.show()
-    mid_new = arry2mid(result_array, 545455)
-    mid_new.save('mid_new.mid')
+
+    rootdir = 'C:\\Projects\\493_GAN\\RenyiGan-TensorFlow2\\archive'
+    destdir = 'C:\\Projects\\493_GAN\\RenyiGan-TensorFlow2\\np_arrays'
+    extension = '.mid'
+    idx = 1
+    for subdir, dirs, files in os.walk(rootdir):
+        for file in files:
+            ext = os.path.splitext(file)[-1].lower()
+            if ext in extension:
+                path = os.path.join(subdir, file)
+                #print(path)
+                mid = mido.MidiFile(path, clip=True)
+                result_array = mid2arry(mid)
+
+                if np.shape(result_array)[0] < 10000:
+                    continue
+                    
+                result_array = result_array[0:10000, :]
+
+                np.savetxt(destdir + '\\' + str(idx) + '.csv', result_array, delimiter=",")
+                idx += 1
+
+                # Legacy code
+                print(np.shape(result_array))
+                # plt.plot(range(result_array.shape[0]), np.multiply(np.where(result_array > 0, 1, 0), range(1, 89)), marker='.',
+                #          markersize=1, linestyle='')
+                # plt.title("test.mid")
+                # plt.show()
+                # mid_new = arry2mid(result_array, 545455)
+                # mid_new.save('mid_new.mid')
+
+
 
 
 
