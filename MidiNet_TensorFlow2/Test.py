68280c994b0f1de929265bd565609ba8067b0c34
aidanby <aidanbyang@hotmail.com>
2021-02-07 12:00:25 -0700

    Upsampling the generator + corresponding discriminator

--- MidiNet_TensorFlow2/Test.py
+++ MidiNet_TensorFlow2/Test.py
@@ -1,12 +1,25 @@
-import numpy as np 
-
-
+import numpy as np
 import os
 import tensorflow as tf
 import matplotlib.pyplot as plt
 import time
 from model import get_generator, get_discriminator
 
+# Aidan's code
+generator = get_generator()
+noise = tf.random.normal([1, 100])
+generated_image = generator(noise, training=False)
+plt.imshow(generated_image[0, :, :, 0], cmap='gray')
 
-G = get_discriminator()
-tf.keras.utils.plot_model(G, to_file="Discirminator.png", show_shapes=True)
+discriminator = get_discriminator()
+decision = discriminator(generated_image)
+print(decision)
+
+
+# Jesse's code
+# G = get_discriminator()
+# plt.imshow(generated_image[0, :, :, 0], cmap='gray')
+# tf.keras.utils.plot_model(G, to_file="Discriminator.png", show_shapes=True)
+
+
+
