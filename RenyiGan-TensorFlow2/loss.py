3509aa7bb22022bf9b6d489f729e1ebb27745ed5
will1032 <55930335+will1032@users.noreply.github.com>
2021-03-03 14:32:38 -0500

    Update loss.py
    added L1 loss for disc

--- RenyiGan-TensorFlow2/loss.py
+++ RenyiGan-TensorFlow2/loss.py
@@ -22,6 +22,11 @@ def generator_loss_renyi(fake_output, alpha):
     loss = 1.0 / (alpha - 1.0) * tf.math.log(f)
     return loss
 
+def generator_loss_renyiL1(fake_output, alpha):
+    f = tf.math.reduce_mean(tf.math.pow(1 - fake_output,
+                                        (alpha - 1) * tf.ones_like(fake_output)))
+    gen_loss = tf.math.abs(1.0 / (alpha - 1) * tf.math.log(f + 1e-8) + tf.math.log(2.0))
+    return gen_loss
 
 def discriminator_loss_renyi(real_output, fake_output, alpha):
     f = (tf.math.pow(real_output, alpha) * \
