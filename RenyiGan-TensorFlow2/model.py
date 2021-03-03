6cc55956256a7bbf5d664ab2f3b4f84db2cffe4f
will1032 <55930335+will1032@users.noreply.github.com>
2021-03-03 14:31:08 -0500

    Update model.py
    added himesh gen disc model

--- RenyiGan-TensorFlow2/model.py
+++ RenyiGan-TensorFlow2/model.py
@@ -38,5 +38,54 @@ def get_discriminator():
 
     return model
 
+# HIMESH MODELS  
+# much more stable
 
+def build_generator():
+        with tf.name_scope('generator') as scope:
+            model = Sequential(name=scope)
+            model.add(Dense(7 * 7 * 256, use_bias=False, kernel_initializer=
+            RandomNormal(mean=0.0, stddev=0.01), input_shape=(28 * 28,)))
+            model.add(BatchNormalization())
+            model.add(LeakyReLU())
+
+            model.add(Reshape((7, 7, 256)))
+            assert model.output_shape == (None, 7, 7, 256)  # Note: None is the batch size
+
+            model.add(Conv2DTranspose(128, (5, 5), strides=(1, 1), padding='same', use_bias=False, kernel_initializer=
+            RandomNormal(mean=0.0, stddev=0.01)))
+            assert model.output_shape == (None, 7, 7, 128)
+            model.add(BatchNormalization())
+            model.add(LeakyReLU())
+
+            model.add(Conv2DTranspose(64, (5, 5), strides=(2, 2), padding='same', use_bias=False, kernel_initializer=
+            RandomNormal(mean=0.0, stddev=0.01)))
+            assert model.output_shape == (None, 14, 14, 64)
+            model.add(BatchNormalization())
+            model.add(LeakyReLU())
+
+            model.add(Conv2DTranspose(1, (5, 5), strides=(2, 2), padding='same', activation='tanh', use_bias=False,
+                                      kernel_initializer=RandomNormal(mean=0.0, stddev=0.01)))
+            assert model.output_shape == (None, 28, 28, 1)
+
+            return model
+
+def build_discriminator():
+        with tf.name_scope('discriminator') as scope:
+            model = Sequential(name=scope)
+            model.add(Conv2D(64, (5, 5), strides=(2, 2), padding='same', kernel_initializer=
+            RandomNormal(mean=0.0, stddev=0.01)))
+            model.add(LeakyReLU())
+            model.add(Dropout(0.3))
+
+            model.add(Conv2D(128, (5, 5), strides=(2, 2), padding='same', kernel_initializer=
+            RandomNormal(mean=0.0, stddev=0.01)))
+            model.add(LeakyReLU())
+            model.add(Dropout(0.3))
+
+            model.add(Flatten())
+            model.add(Dense(1, activation='sigmoid', kernel_initializer=
+            RandomNormal(mean=0.0, stddev=0.01)))
+
+            return model
 
